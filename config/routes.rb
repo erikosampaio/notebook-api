a9751ea30c58a6f547d9cf3f71ab54e513fe6fc8
erikosampaio <erikoa.93@gmail.com>
2022-10-24 15:50:30 -0300

    Crio CRUD para phones a partir de contact

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phone'
+
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
 
