fa92cdfa8b76de6c374a62bbecf70e15d2f19726
erikosampaio <erikoa.93@gmail.com>
2022-10-25 17:03:08 -0300

    Utilizo autenticação http basic e digest

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,4 +1,12 @@
 class KindsController < ApplicationController
+  
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name:"eriko", password:"123"
+
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = { "eriko" => Digest::MD5.hexdigest(["eriko", "Application", "123"].join(":")) }
+
+  before_action :authenticate  
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -52,5 +60,11 @@ class KindsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
     end
 end
